df02efae51149ed21ca31a76c301205a7168b6c0
Vera Vinken (PGR) <c0070955@newcastle.ac.uk>
2022-06-22 15:15:17 +0100

    Comments on connecting R-git-github

--- Rcode/testScript.R
+++ Rcode/testScript.R
@@ -2,3 +2,40 @@
 library(ggplot)
 
 print('this is a test')
+
+
+# So to connect r to your github
+
+# go to github and create the repository 
+# copy the url o fthe new repo 
+# go to r and create a new project
+# chose version control and click GIT
+# paste hte URL in and put in the righ tfolder
+
+# I have personally put it in th H folder under modelling
+# unfortunately, it doesnt connec tto hte labfolder
+
+# afte rputtin gin pasword this should appear now
+
+# create new files that are saved in a folder (right bottom)
+# make sure to group Rcode in the folder that contains the code
+
+# Then we created a new scirpt (this one)
+
+# To commit to online github. there are 2 steps
+
+# 1: commit locally
+# 2: commit online
+
+# first we go to the 'git' tab next to the environment
+# there we click the documents/paths that we want to commit
+# BiPhD-ABM.proj is lcoal (dont understand this really)
+# we commit the readme (changes wer made)
+# and the code we wrote (this script)
+
+# The next step is to push the commit to the central github 
+# so far, the git software has been told about the chagnes
+# now we tell the main repository abot the changes
+# to do so: click the green upwards arrow 
+
+# done! 
