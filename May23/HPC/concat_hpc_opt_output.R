f7b69c6255bcd171a968a8052dc3c50db00985e6
Vera-Anne <veraannevinken@gmail.com>
2023-08-31 17:49:55 +0100

    concatenating is working

--- May23/HPC/concat_hpc_opt_output.R
+++ May23/HPC/concat_hpc_opt_output.R
@@ -9,24 +9,28 @@
 
 
 # load packages 
+library('parallel')
+library('doParallel')
+library(dplyr)
+library(plotly)         # For the 3D scatterplot 
+library(ggplot2)
 
 
 
-# set type of optimization 
-opt_type<-131
 
 # Set the folder in which the results are (this is the folder that contains the batches with results)
 batch_folder<-'C:/Users/c0070955/OneDrive - Newcastle University/1-PHD-project/Modelling/R/Model_output/HPC/131/2023-08-26/'
+  # 131: 
+  # 132:  'C:/Users/c0070955/OneDrive - Newcastle University/1-PHD-project/Modelling/R/Model_output/HPC/132/2023-08-29/'
+
+# set opt_type
+opt_type=131
 
 
+############################
+# FOR X.3.X OPTIMIZATIONS  #
+############################
 
-
-# IF statement : depenidng on opt-type this code differs 
-
-if (opt_type==131){
-
-    # If opt_type == 131 
-    
       # navigate to folder where the 10 folders with the batches are (specified above)
       setwd(paste0(batch_folder))
       
@@ -51,72 +55,66 @@ if (opt_type==131){
             # make halflife list
             halflife_list<-list()
             
+            numCores<-(detectCores()-1)
+            registerDoParallel(numCores)
+            
+            outcome_concat<- foreach(j=1:length(filenames), .combine='c') %dopar% {
+            
             # For each of the files in the current batch: extract the halflife 
-            for (j in 1:length(filenames)){
+            # for (j in 1:length(filenames)){
                 # load the current file 
                 load(filenames[j])
                 # extract the current halflife and put in list 
-                halflife_list[j]<-env_results[1]
-                print(paste('batch',i ,'file', j))
-              
+                halflife_list[1]<-env_results[1]
+                halflife_list[[1]][3]<-env_results[[3]]$th1
+                halflife_list[[1]][4]<-env_results[[3]]$th2
+                halflife_list[[1]][5]<-env_results[[3]]$th3
+                halflife_list[[1]][6]<-env_results[[3]]$th_comb_input
+                #print(paste('batch',i ,'file', j))
+                # to add this to teh 'outcome_concat' 
+                halflife_list
             } # end for loop that runs through files in a batch-folder
             
+            # stop the cluster
+            stopImplicitCluster()
+            
             # Turn the list into a dataframe 
-            halflife_df<-as.data.frame(do.call(rbind, halflife_list))
+            halflife_df<-as.data.frame(do.call(rbind, outcome_concat))
+            colnames(halflife_df)<-c('mean', 'sd', 'th1', 'th2', 'th3', 'th_num')
             
             # Add this halflife_list to the list that collects the halflifes per batch 
             halflife_per_batch_list[[i]]<-halflife_df
             
+            print(paste('batch done #', i))
+            
       } # end for loop through all batch-folder names 
+      
+
       
       # The result should be a list that has (in the case of 131) 10 lists inside with the halflifes 
       
       # concatenate them all 
       HL_df<-as.data.frame(do.call(rbind, halflife_per_batch_list))
       
-      # Now add the threshold values that were used for this to the dataframe 
-      require(pracma)
-      num_th<-50
-      # set the minima
-      min_th_1<-0
-      min_th_2<-0
-      min_th_3<-0
-      # set the maxima
-      max_th_1<-0.4
-      max_th_2<-0.4
-      max_th_3<-0.4
-      # create the vectors
-      th1_vec<-linspace(x1=min_th_1, x2=max_th_1, n=num_th)
-      th2_vec<-linspace(x1=min_th_2, x2=max_th_2, n=num_th)
-      th3_vec<-linspace(x1=min_th_3, x2=max_th_3, n=num_th)
-      # Turn into dataframe 
-      th1_th2_th3_comb<-as.data.frame(as.matrix(expand.grid(th1_vec, th2_vec, th3_vec)))
+      # Calculate the best value 
+      HL_best<-HL_df[(which.max(HL_df$mean)),]
       
+      # Save in the folder
+      save(HL_df, file=paste('opt_outcome_concat_HPC_', opt_type, '.Rda'))
       
-      # Add a column that checks if these are 'sensible' combinations 
-      for (i in 1:nrow(th1_th2_th3_comb)){
-        if (th1_th2_th3_comb$Var3[i] > th1_th2_th3_comb$Var2[i] && th1_th2_th3_comb$Var2[i] > th1_th2_th3_comb$Var1[i]){
-          th1_th2_th3_comb$test[i]<-1
-        } else{
-          th1_th2_th3_comb$test[i]<-0
-        }
-      }
+      HL_df$th1<-as.numeric(HL_df$th1)
+      HL_df$th2<-as.numeric(HL_df$th2)
+      HL_df$th3<-as.numeric(HL_df$th3)
+      HL_df$mean<-as.numeric(HL_df$mean)
+      HL_df$th_num<-as.numeric(HL_df$th_num)
       
-      # now select 
-      sensible_combs<-subset(th1_th2_th3_comb, th1_th2_th3_comb$test>0)
+      # order the data 
+      HL_df_ordered<-HL_df[order(HL_df$th_num),]
       
-      # Bind them together 
-      HL_df<-cbind(HL_df, sensible_combs)
-      
-      # calculate the best HL across the different combinations 
-      HL_best<-HL_df[(which.max(HL_df)),]
-      
-} else if (opt_type==132){
-  
-} else if (opt_type==231){
-  
-} else {
-  print( 'define opt_type')
-}
-      
-      
+      # halflife
+      plot_ly(HL_df_ordered, x = ~th1, y = ~th2, z = ~th3, color = ~mean) %>%
+        add_markers(size=~mean, marker=list(sizeref=0.02, sizemode='area')) %>%
+        layout(scene = list(xaxis = list(range=c(0, 0.4),title = 'TH1'),
+                            yaxis = list(range=c(0, 0.4),title = 'TH2'),
+                            zaxis = list(range=c(0, 0.4),title = 'TH3')),
+               title = list(text='1.3.1 HPC Mean Halflife - 3 thresholds ', y=0.95))    
