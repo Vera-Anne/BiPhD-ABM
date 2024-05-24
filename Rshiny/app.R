2e19e36bfb700e0ee100021ba72aef40d4631085
Vera-Anne <veraannevinken@gmail.com>
2024-05-24 20:06:04 +0100

    Works for graphs and sizing + colours implemented

--- Rshiny/app.R
+++ Rshiny/app.R
@@ -2,19 +2,9 @@
 # Vera Vinken 
 # 20 May 2014 
 
-
-#### TO DO LIST #### 
-
-# make the check boxes for variable and environment drop downs 
-# fix that graphics box that turns up 
-# figure sizes
-# Model selection should go by "all hoarders", "all direct hoarders", "all stomach content" 
-# Split up subset 1 and subset 2? 
-# Fix colours so that each model has its own color that it will always get
-
-
 # Packages 
 library(shiny)
+library(shinyWidgets)
 library(ggplot2)
 library(dplyr)
 library(plotly)
@@ -29,16 +19,30 @@ rm(list = ls())
 # UI - USER INTERFACE
 ###############################################
 
+
 ui <- fluidPage(
+  # set background
+  setBackgroundColor("#e3e2db"),
   # Header with logo 
-  titlePanel(title=div(img(src="logo.png", height = "5%", width= "5%"), "Tit Simulator")),
+  titlePanel(title=div(img(src="logo.png", height = "3%", width= "3%"), "Tit Simulator", 
+                       style = "background-color: #484537; padding: 12px; color:#e3e2db; font-size:35px; font-style:bold;")),
+  
   
   # Create the sidebar for variable input 
-  
   sidebarLayout(
     # panel to the left
     sidebarPanel(
-      # Question 1: which models? 
+      style = "background-color: #c8c4b7; padding: 15px; font-size:15px;",
+      tags$head(
+        tags$style(type='text/css', 
+                   ".nav-tabs {font-size: 20px;} 
+              /* Set the height of plot containers */
+              .plot-container {
+                height: 80vh; /* 80% of the viewport height */
+              }"
+        )
+      ),
+      
       checkboxGroupInput("models_to_show", "Pick which models to show:",
                          c("1.1 - Non hoarding - SC" = 11, 
                            "1.2 - Leftover hoarding - SC" = 12,
@@ -63,152 +67,133 @@ ui <- fluidPage(
                            "6.1 - Non hoarding - FCR" = 61, 
                            "6.2 - Leftover hoarding - SC & FCR" = 62,
                            "6.3.1 - Direct hoarding (H top) - SC & FCR (DOESNT WORK YET)" = 631, 
-                           "6.3.2 - Direct hoarding (R top) - SC & FCR" = 632)
-                         ,select = c(11)
-                         ),
-      # Question 2: which environments for survival? 
-        selectInput("environments_to_show",
-        label = "Pick which environment selection to show",
-        choices = 
-          c(
-            "Mean across all" = "mean", 
-            "All 12 seperate" = "all"
-          ),
-        selected = "Mean across all"),
-      # Question 3: which physiological variable? 
+                           "6.3.2 - Direct hoarding (R top) - SC & FCR " = 632),
+                         selected = c(11)
+      ),
+      selectInput("environments_to_show",
+                  label = "Pick which environment selection to show",
+                  choices = 
+                    c(
+                      "Mean across all" = "mean", 
+                      "All 12 separate" = "all"
+                    ),
+                  selected = "all"),
       selectInput("phys_var_to_show", 
                   label = "Select the physiological variable", 
                   choices = c("Stomach content" = "stom_con", 
                               "Fat reserve" = "fat_res", 
                               "Fat change rate" = "fat_loss_r"), 
                   selected = "stom_con"),
-      # Question 4: which behaviour variable? 
       selectInput("beh_var_to_show", 
                   label = "Select the behaviour variable", 
                   choices = c("Eat" = "eat", 
                               "Eat & hoard" = "eat_hoard", 
-                              #"Foraging attempt" = "forage", 
                               "Hoard direct" = "dir_hoard", 
-                              #"Successful foraging attempt" = "find_food", 
                               "Rest" = "rest", 
                               "Retrieve cache" = "retrieve", 
                               "Sleep" = "sleep"), 
-                  selected = "eat")
+                  selected = "eat"),
+      # sliderInput("height", "Plot height", min = 100, max = 1800, value = 800),
+      # sliderInput("width", "Plot width", min = 100, max = 1800, value = 1100),
+      actionButton(inputId = "go", label = "Update")
     ),
     # Print the output in the main panel 
     mainPanel(
-      height = "95%", 
+      style = "height: 90vh; overflow-y: auto;",
+      tags$head(
+        tags$style(type='text/css', 
+                   ".nav-tabs {font-size: 20px;} 
+              # /* Set the height of plot containers */
+              # .plot-container {
+              #   height: 80vh; /* 80% of the viewport height */
+              # }"
+        )
+      ),
+      tabsetPanel(
+        #style = h
+        id = "tabset",
+        tabPanel("Survival", plotlyOutput("survplot", height = "100%", width = "100%"),style = "background-color:#ffffff; padding: 15px;height:80vh;"),
+        tabPanel("Physiological variable", plotlyOutput("physplot", height = "100%", width = "100%"), style = "background-color:#ffffff; padding: 15px;height:80vh;"),
+        tabPanel("Behavioural variable", plotlyOutput("behplot", height = "100%", width = "100%"), style = "background-color:#ffffff; padding: 15px;height:80vh;")
+      ) # close tabset
+
+
       
-      # Tabs with information
-      navset_card_underline(
-        # For the survival plots 
-        nav_panel("Survival", plotlyOutput("survplot", height = "100%", width = "100%")),
-        
-        nav_panel("Physiological variable", plotlyOutput("physplot", height = 800, width = "100%")),
-        
-        nav_panel("Behaviour variable", plotlyOutput("behplot", height = 800, width = "100%")),
-        
-        nav_panel("Text 2", textOutput("env_chosen_text"))
-      )
       
-    )
-  ) 
+    ) # close main panel
+  ), # close sidebar layout
   
- 
   
-  # Start the sidebar (panel on the left)
   
-)
-
-##################################################
-# SERVER 
-##################################################
-
-# input<-list(
-#   environments_to_show = 1,
-#   models_to_show = c(11, 21),
-#   phys_var_to_how = "fat_res"
-# )
+  
+) # close fluidpage
 
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
   # Load the default data from the .rda file
   load("data/results.rda") 
-  #load("data/Esum_results.rda")
   load("data/Dsum_results.rda")
   
   # Load the colors and themes
   source("R/colours_themes.R")
   
-  
   # Create the colour scale 
-  #Create a custom color scale
   myColors <- stepped()
   names(myColors) <- levels(df_out$model)
-  colScale <- scale_colour_manual(name = "model",values = myColors)
-  
+  colScale <- scale_colour_manual(name = "model", values = myColors)
   
   # Reactive statement for the data selection 
-  data<-reactive({
+  data <- eventReactive(input$go, {
     # In all cases, select the models needed
-    df<-df_out%>%
-      filter(model %in% input$models_to_show)%>%
+    df <- df_out %>%
+      filter(model %in% input$models_to_show) %>%
       filter(id == "alive")
-    # Check if we need to summarise because 
-    if(input$environments_to_show == "all"){
+    # Check if we need to summarise
+    if(input$environments_to_show == "all") {
       # Nothing needs to happen
-    }else if(input$environments_to_show == "mean"){
+    } else if(input$environments_to_show == "mean") {
       # We need to summarise across all 12 environments 
-      df<-df%>%
-        group_by(timestep, id, model)%>%
-        summarise(mean_val_env = mean(value, na.rm=T), nObs =n())%>%
+      df <- df %>%
+        group_by(timestep, id, model) %>%
+        summarise(mean_val_env = mean(value, na.rm = TRUE), nObs = n()) %>%
         ungroup()
     }
-    # This is where the other options (across temp, across food) will be. 
-    
-    # Feed back the data frame 
-    df
-    })
-  
-  # Reactive data for the behavioral and physiological data 
-  # They need to be summarized across the days
-  # And then according to the environment selection
-  
-  data_Dsum<-reactive({
-    # TAke out models that we don't use 
-    df<-df_out_Dsum%>%
-      filter(model %in% input$models_to_show)
-    # This data is in principle ready to be used across days
-    # But if any of the "means" is selected, it will need to be changed
-    if(input$environments_to_show=="all"){
-      # nothing needs to happen
-    }else if(input$environments_to_show == "mean"){
-      # I need to take the mean across all environments 
-      df<-df%>%
-        group_by(timestep_within_day, id, model)%>%
-        summarise(mean_val_env = mean(mean_val_day, na.rm=T), nObs=n())%>%
-        ungroup()
-    }
-    # This is where I@ll put the other environment grouping options 
-    
-    # return the data fram 
     df
   })
-
-data_phys<-reactive(
-  data_Dsum()%>%
-    filter(id == paste(input$phys_var_to_show))
-)
-
-data_beh<-reactive(
-  data_Dsum()%>%
-    filter(id == paste(input$beh_var_to_show))
-)
   
-
-# Render the survival plot
+  # Reactive data for the behavioral and physiological data 
+  data_Dsum <- eventReactive(input$go, {
+    # Take out models that we don't use 
+    df <- df_out_Dsum %>%
+      filter(model %in% input$models_to_show)
+    # This data is in principle ready to be used across days
+    if(input$environments_to_show == "all") {
+      # nothing needs to happen
+    } else if(input$environments_to_show == "mean") {
+      # Take the mean across all environments 
+      df <- df %>%
+        group_by(timestep_within_day, id, model) %>%
+        summarise(mean_val_env = mean(mean_val_day, na.rm = TRUE), nObs = n()) %>%
+        ungroup()
+    }
+    df
+  })
+  
+  # phys specific 
+  data_phys <- eventReactive(input$go, {
+    data_Dsum() %>%
+      filter(id == paste(input$phys_var_to_show))
+  })
+  
+  # beh specific 
+  data_beh <- eventReactive(input$go, {
+    data_Dsum() %>%
+      filter(id == paste(input$beh_var_to_show))
+  })
+  
+  # Render the survival plot
   output$survplot <- renderPlotly({
-    # Prepare the survival plot
     p <- if(input$environments_to_show == "mean") {
       ggplot(data(), aes(x = timestep, y = mean_val_env)) +
         geom_line(aes(col = model)) +
@@ -219,16 +204,15 @@ data_beh<-reactive(
               legend.text = element_text(size = 10), 
               legend.title = element_blank()) +
         ggtitle("Survival") +
-        xlab(label = "Timestep (20 min)")+
-        ylab(label = "Proportion  of birds alive")+
-        vera_theme()+
-        #scale_color_manual(values=as.vector(kelly(24))) +
-        colScale+
+        xlab("Timestep (20 min)") +
+        ylab("Proportion of birds alive") +
+        vera_theme() +
+        colScale +
         labs(fill = " ")
     } else {
       ggplot(data(), aes(x = timestep, y = value)) +
         geom_line(aes(col = model)) +
-        facet_wrap(.~env, nrow = 6) +
+        facet_wrap(. ~ env, nrow = 6) +
         theme(plot.title = element_text(face = 'bold', size = 25), 
               axis.title.x = element_text(face = 'bold', size = 15), 
               axis.title.y = element_text(face = 'bold', size = 15), 
@@ -236,99 +220,88 @@ data_beh<-reactive(
               legend.text = element_text(size = 10), 
               legend.title = element_blank()) +
         ggtitle("Survival") +
-        xlab(label = "Timestep (20 min)")+
-        ylab(label = "Proportion  of birds alive")+
-        vera_theme()+
-        #scale_color_manual(values=as.vector(kelly(24))) +
-        colScale+
+        xlab("Timestep (20 min)") +
+        ylab("Proportion of birds alive") +
+        vera_theme() +
+        colScale +
         labs(fill = " ")
     }
-    
-    ggplotly(p)
+    plotlyOutput <- ggplotly(p)#, width = input$width, height = input$height)#, height = input$height, width = input$width)
+    plotlyOutput
   })
-  
   
   # Render the physiological variable plot 
   output$physplot <- renderPlotly({
-    # Prepare the phys plot
     p <- if(input$environments_to_show == "mean") {
       ggplot(data_phys(), aes(x = timestep_within_day, y = mean_val_env)) +
         geom_line(aes(col = model)) +
-        theme(plot.title = element_text(face = 'bold', size = 25), 
+        theme(plot.title = element_text(face = 'bold', size = 20), 
               axis.title.x = element_text(face = 'bold', size = 15), 
               axis.title.y = element_text(face = 'bold', size = 15), 
               strip.text = element_text(), 
               legend.text = element_text(size = 10), 
               legend.title = element_blank()) +
         ggtitle(paste("Physiological variable = ", input$phys_var_to_show)) +
-        xlab(label = "Timestep (20 min) within Day")+
-        ylab(label = paste(input$phys_var_to_show))+
-        vera_theme()+
-        #scale_color_manual(values=as.vector(kelly(24))) +
-        colScale+
+        xlab("Timestep (20 min) within Day") +
+        ylab(input$phys_var_to_show) +
+        vera_theme() +
+        colScale +
         labs(fill = " ")
     } else {
-      p <- ggplot(data_phys(), aes(x = timestep_within_day, y = mean_val_day)) +
+      ggplot(data_phys(), aes(x = timestep_within_day, y = mean_val_day)) +
         geom_line(aes(col = model)) +
-        facet_wrap(.~env, nrow = 6) +
-        theme(plot.title = element_text(face = 'bold', size = 25), 
+        facet_wrap(. ~ env, nrow = 6) +
+        theme(plot.title = element_text(face = 'bold', size = 20), 
               axis.title.x = element_text(face = 'bold', size = 15), 
               axis.title.y = element_text(face = 'bold', size = 15), 
               strip.text = element_text(), 
               legend.text = element_text(size = 10), 
               legend.title = element_blank()) +
         ggtitle(paste("Physiological variable = ", input$phys_var_to_show)) +
-        xlab(label = "Timestep (20 min) within Day")+
-        ylab(label = paste(input$phys_var_to_show)) +
-        vera_theme()+
-        #scale_color_manual(values=as.vector(kelly(24))) +
-        colScale+
+        xlab("Timestep (20 min) within Day") +
+        ylab(input$phys_var_to_show) +
+        vera_theme() +
+        colScale +
         labs(fill = " ")
     }
-    
-    ggplotly(p)
+    ggplotly(p)#, width = input$width, height = input$height)
   })
-  
   
   # Render the behaviour variable plot 
   output$behplot <- renderPlotly({
-    # Prepare the beh plot
     p <- if(input$environments_to_show == "mean") {
       ggplot(data_beh(), aes(x = timestep_within_day, y = mean_val_env)) +
         geom_line(aes(col = model)) +
-        theme(plot.title = element_text(face = 'bold', size = 25), 
+        theme(plot.title = element_text(face = 'bold', size = 20), 
               axis.title.x = element_text(face = 'bold', size = 15), 
               axis.title.y = element_text(face = 'bold', size = 15), 
               strip.text = element_text(), 
               legend.text = element_text(size = 10), 
               legend.title = element_blank()) +
         ggtitle(paste("Behaviour variable = ", input$beh_var_to_show)) +
-        xlab(label = "Timestep (20 min) within Day")+
-        ylab(label = paste(input$beh_var_to_show))+
-        vera_theme()+
-        #scale_color_manual(values=as.vector(kelly(24))) +
-        colScale+
+        xlab("Timestep (20 min) within Day") +
+        ylab(input$beh_var_to_show) +
+        vera_theme() +
+        colScale +
         labs(fill = " ")
     } else {
-      p <- ggplot(data_beh(), aes(x = timestep_within_day, y = mean_val_day)) +
+      ggplot(data_beh(), aes(x = timestep_within_day, y = mean_val_day)) +
         geom_line(aes(col = model)) +
-        facet_wrap(.~env, nrow = 6) +
-        theme(plot.title = element_text(face = 'bold', size = 25), 
+        facet_wrap(. ~ env, nrow = 6) +
+        theme(plot.title = element_text(face = 'bold', size = 20), 
               axis.title.x = element_text(face = 'bold', size = 15), 
               axis.title.y = element_text(face = 'bold', size = 15), 
               strip.text = element_text(), 
               legend.text = element_text(size = 10), 
               legend.title = element_blank()) +
         ggtitle(paste("Behavioural variable = ", input$beh_var_to_show)) +
-        xlab(label = "Timestep (20 min) within Day")+
-        ylab(label = paste(input$beh_var_to_show)) +
-        vera_theme()+
-        #scale_color_manual(values=as.vector(kelly(24))) +
-        colScale+
+        xlab("Timestep (20 min) within Day") +
+        ylab(input$beh_var_to_show) +
+        vera_theme() +
+        colScale +
         labs(fill = " ")
     }
-    
-    ggplotly(p)
+    ggplotly(p)#, width = input$width, height = input$height)
   })
   
   # Output the selected models
@@ -341,5 +314,6 @@ data_beh<-reactive(
     paste0("The environments chosen are: ", paste(input$environments_to_show, collapse = ";"))
   })
 }
+
 
 shinyApp(ui, server)
