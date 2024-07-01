f1499e992ccff119e0a728c0aa3ebd06cb54bc85
Vera Vinken (PGR) <c0070955@newcastle.ac.uk>
2024-07-01 11:23:06 +0100

    amended modelrun HPC so it doesnt include hte individual runs. Now for each model.

--- Parameterization/binary_branching/predesigned_branch/HPC_runModel.R
+++ Parameterization/binary_branching/predesigned_branch/HPC_runModel.R
@@ -110,6 +110,8 @@ if (modelType==0){
   
   #add this to the output list
   env_results[[3]]<-args
+  env_results[[2]]<-"Removed indiv results to save space  "
+  
   # save the data 
   setwd(out_dir)
   # make sure to attach the threshold to the dataframe 
@@ -148,6 +150,8 @@ if (modelType==0){
   
   #add this to the output list
   env_results[[3]]<-args
+  env_results[[2]]<-"Removed indiv results to save space  "
+  
   
   # save the data 
   setwd(out_dir)
@@ -201,6 +205,8 @@ if (modelType==0){
   stopImplicitCluster()
   #add this to the output list
   env_results[[3]]<-args
+  env_results[[2]]<-"Removed indiv results to save space  "
+  
   # save the data 
   setwd(out_dir)
   # make sure to attach the threshold to the dataframe 
@@ -243,6 +249,8 @@ if (modelType==0){
   env_results
   #add this to the output list
   env_results[[3]]<-args
+  env_results[[2]]<-"Removed indiv results to save space  "
+  
   # save the data 
   setwd(out_dir)
   save(env_results, file=paste0('out_', modelType, '_HPC_',"_lev", level, '_THrow', th_row,"_rep", rep_num,'.Rda'))
@@ -286,6 +294,8 @@ if (modelType==0){
   env_results
   #add this to the output list
   env_results[[3]]<-args
+  env_results[[2]]<-"Removed indiv results to save space  "
+  
   # save the data 
   setwd(out_dir)
   save(env_results, file=paste0('out_', modelType, '_HPC_', "_lev", level, '_THrow', th_row,"_rep", rep_num,'.Rda'))
@@ -340,7 +350,9 @@ if (modelType==0){
   env_results
   #add this to the output list
   env_results[[3]]<-args
-  env_results[[2]]<-"Removed indiv results to save space"
+  
+  env_results[[2]]<-"Removed indiv results to save space  "
+  
   # save the data 
   setwd(out_dir)
   save(env_results, file=paste0('out_', modelType, '_HPC_', "_lev", level, '_THrow', th_row,"_rep", rep_num,'.Rda'))
